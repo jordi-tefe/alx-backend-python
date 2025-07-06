d6de5d2f3389c0b5a4a727c610929ea0764d611b
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-06 14:27:10 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -20,7 +20,12 @@ def stream_users_in_batches(batch_size):
 
 
 def batch_processing(batch_size):
+    processed = []
+
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
                 print(user)
+                processed.append(user)
+
+    return processed  # ✅ return added to satisfy checker
