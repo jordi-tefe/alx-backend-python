02809d433b709802eda39ef432dd3726749c72e8
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-06 13:52:25 +0300

    Create 1-batch_processing.py

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,26 @@
+def stream_users_in_batches(batch_size):
+    import seed
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM user_data")
+    batch = []
+
+    for row in cursor:
+        batch.append(row)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+
+    if batch:  # yield remaining rows if any
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
