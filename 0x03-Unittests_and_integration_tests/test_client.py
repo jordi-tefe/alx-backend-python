bcf5e8f2838dd40d5f20593acead7dd000be5a70
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-19 14:33:20 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -53,6 +53,15 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/orgs/test_org/repos"
             )
 
+        @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that has_license returns expected boolean"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
     def test_public_repos_url(self):
         """Test that _public_repos_url returns expected repos URL"""
         with patch('client.GithubOrgClient.org',
