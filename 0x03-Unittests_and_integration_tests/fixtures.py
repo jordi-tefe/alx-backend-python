9ff6d8293ffa32f27bc2022d7da7dfab9cbbeb44
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-19 14:44:05 +0300

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -948,3 +948,18 @@ TEST_PAYLOAD = [
     ['dagger', 'kratu', 'traceur-compiler', 'firmata.py'],
   )
 ]
+org_payload = {
+    "login": "test_org",
+    "repos_url": "https://api.github.com/orgs/test_org/repos"
+}
+
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
+]
+
+expected_repos = ["repo1", "repo2", "repo3"]
+
+apache2_repos = ["repo1", "repo3"]
+
