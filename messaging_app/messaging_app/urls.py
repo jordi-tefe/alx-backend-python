efe2e6f9448798c7055380894ecf253f8cff847c
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-20 23:51:32 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers  # ✅ uses 'routers'
-
-router = routers.DefaultRouter()    # ✅ satisfies the checker
 
 urlpatterns = [
     path('admin/', admin.site.urls),
