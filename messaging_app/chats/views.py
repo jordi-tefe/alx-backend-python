6e3c3aae74046ef42e9cb4517b3cb6752c1a9e5c
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-20 23:39:50 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Will implement API views in later tasks
+from rest_framework import generics, permissions
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+# ✅ List conversations for the authenticated user
+class UserConversationsView(generics.ListAPIView):
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
+
+# ✅ List messages in a specific conversation
+class ConversationMessagesView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs['conversation_id']
+        return Message.objects.filter(conversation__conversation_id=conversation_id)
+
+# ✅ Send a message (Create new message)
+class SendMessageView(generics.CreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
