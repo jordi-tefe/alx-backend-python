7db0e3c11029873627c3093a1fb551fcf1ce9310
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-20 23:02:35 +0300

    Create settings.py

---
+++ messaging_app/messaging_app/settings.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # 3rd Party
+    'rest_framework',
+
+    # Your App
+    'chats',
+]
