6068a4edde520d02d4ad7d9ebceda426ec867d56
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-27 17:23:58 +0300

    Update pagination.py

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -2,5 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class MessagePagination(PageNumberPagination):
     page_size = 20
-    page_size_query_param = 'page_size'  # Optional: client can set ?page_size=10
+    page_size_query_param = 'page_size'  # optional
     max_page_size = 100
