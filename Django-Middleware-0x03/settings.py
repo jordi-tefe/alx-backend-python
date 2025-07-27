29f881bc1c68f70519c407b491441e63e7b460d1
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-27 22:04:07 +0300

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'chats.middleware.ThrottlingMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
