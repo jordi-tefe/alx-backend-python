2658e6df4ee932a322daceffd792234476d298d4
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-27 21:06:42 +0300

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -26,3 +26,23 @@ class OffensiveLanguageMiddleware:
 
     def get_client_ip(self, request):
         return request.META.get('REMOTE_ADDR')
+        # chats/middleware.py
+
+import logging
+
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('requests.log')
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        logger.info(f"{request.method} {request.path}")
+        response = self.get_response(request)
+        return response
+
