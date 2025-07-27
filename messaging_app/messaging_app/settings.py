9fc3d544ebc1b78736c4473e6f3a3c1c08384382
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-27 16:50:14 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -13,11 +13,12 @@ INSTALLED_APPS = [
     'chats',
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ),
 }
+
 AUTH_USER_MODEL = 'chats.User'
