d67f2509ba8ab6fb66331a0dc51ff42426a7030c
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-27 17:20:40 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -104,6 +104,11 @@ AUTH_USER_MODEL = 'chats.User'
 
 # ✅ DRF & JWT Setup
 REST_FRAMEWORK = {
+      'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': ['messaging_app.chats.pagination.MessagePagination',
+    'PAGE_SIZE': 20],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
