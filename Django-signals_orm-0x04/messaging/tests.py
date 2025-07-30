df4f4e7ca6a265c3d4aa39ebcfd7a9bc89bb758e
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-30 13:15:39 +0300

    Update tests.py

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -9,3 +9,11 @@ class SignalTests(TestCase):
         Message.objects.create(sender=sender, receiver=receiver, content='Hello')
 
         self.assertEqual(Notification.objects.count(), 1)
+def test_log_on_update(self):
+    sender = User.objects.create_user(username='sender')
+    receiver = User.objects.create_user(username='receiver')
+    message = Message.objects.create(sender=sender, receiver=receiver, content='Initial')
+    
+    message.content = 'Updated message'
+    message.save()
+    # The log will be printed to the console when this test runs
