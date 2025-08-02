5deab3e593037d7c012e2bd7dc896879a10a8f20
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-08-02 13:41:31 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -6,7 +6,6 @@ from .models import MessageHistory,Message
 from .serializers import MessageHistorySerializer,MessageSerializer
 from rest_framework.generics import ListAPIView ,RetrieveAPIView
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
@@ -78,11 +77,3 @@ class UnreadInboxView(generics.ListAPIView):
         return Message.unread.unread_for_user(self.request.user).only(
             'id', 'sender', 'content', 'timestamp'
         )
-
-class MessageCreateView(generics.CreateAPIView):
-    serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]  # Important!
-
-    def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
