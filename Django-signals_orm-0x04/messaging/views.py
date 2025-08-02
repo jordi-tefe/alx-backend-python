a3bdfd002597c132f1a0218f8b4a9db1cca5314b
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-08-02 13:01:39 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status , generics, permissions
-from .models import MessageHistory
-from .serializers import MessageHistorySerializer
+from .models import MessageHistory,Message
+from .serializers import MessageHistorySerializer,MessageSerializer
+from rest_framework.generics import ListAPIView
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
@@ -19,3 +20,13 @@ class MessageHistoryView(generics.ListAPIView):
     def get_queryset(self):
         message_id = self.kwargs['message_id']
         return MessageHistory.objects.filter(message__id=message_id)
+
+class MessageThreadView(ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Load all messages received or sent by the user, including replies
+        return Message.objects.filter(
+            sender=self.request.user
+        ).select_related('sender', 'receiver', 'parent_message').prefetch_related('replies')
