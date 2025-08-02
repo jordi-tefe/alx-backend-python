b4cedfe16a3233a4c55891c47ae21112b84b642d
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-08-02 13:02:35 +0300

    Update serializers.py

--- Django-signals_orm-0x04/messaging/serializers.py
+++ Django-signals_orm-0x04/messaging/serializers.py
@@ -10,3 +10,15 @@ class MessageHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MessageHistory
         fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    replies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'receiver', 'content', 'timestamp', 'parent_message', 'replies']
+
+    def get_replies(self, obj):
+        # Recursive fetching of nested replies
+        replies = obj.replies.all().select_related('sender', 'receiver')
+        return MessageSerializer(replies, many=True).data
