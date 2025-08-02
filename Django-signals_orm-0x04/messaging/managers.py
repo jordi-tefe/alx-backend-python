35d027b55b43b966dddd971dcf3bcdbdf9204bc3
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-08-02 13:26:16 +0300

    Create managers.py

---
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class UnreadMessagesManager(models.Manager):
+    def unread_for_user(self, user):
+        return super().get_queryset().filter(receiver=user, read=False)
