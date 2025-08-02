d71eb02f212700380aa1b48f57aa5243457bf244
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-08-02 12:48:03 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,12 +1,11 @@
-from rest_framework import status, permissions
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.contrib.auth.models import User
+from rest_framework import status
 
-class DeleteUserView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def delete(self, request):
-        user = request.user
-        user.delete()
-        return Response({"detail": "User account deleted."}, status=status.HTTP_204_NO_CONTENT)
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_user(request):
+    user = request.user
+    user.delete()
+    return Response({"detail": "User account deleted."}, status=status.HTTP_204_NO_CONTENT)
